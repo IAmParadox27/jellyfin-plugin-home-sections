7cd521c5720460a33ef972f926c7c26ba0f0e523
chandler <c@c.com>
2025-09-06 14:32:46 -0700

    10.11

--- src/Jellyfin.Plugin.HomeScreenSections/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/Properties/AssemblyInfo.cs
@@ -6,5 +6,4 @@ using Jellyfin.Plugin.HomeScreenSections.Attributes;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugin.HomeScreenSections")]
 [assembly: AssemblyVersion("2.3.0.0")]
-[assembly: JellyfinVersion("10.10.7")]
 [assembly: JellyfinVersion("10.11.0-rc5")]
