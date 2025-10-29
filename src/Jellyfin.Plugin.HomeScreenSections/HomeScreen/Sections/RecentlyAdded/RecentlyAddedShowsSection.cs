039d05faa26328c6de81d17ee8ac6d482b182673
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-10-29 23:07:07 +0000

    Another minor optimisation to recently added section

--- src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/RecentlyAdded/RecentlyAddedShowsSection.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/RecentlyAdded/RecentlyAddedShowsSection.cs
@@ -34,6 +34,26 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections.RecentlyAdded
         {
         }
 
+        protected override IEnumerable<BaseItem> GetItems(User? user, DtoOptions dtoOptions, VirtualFolderInfo[] folders, bool? isPlayed)
+        {
+            // Default behaviour is to get the 16 most recently added items from each library that matches, then order that by date created and take 16.
+            // The reason we do this is to ensure that we always get 16 items, even if there is only 1 library that matches our type.
+            return folders.SelectMany(x =>
+            {
+                return m_libraryManager.GetItemList(new InternalItemsQuery(user)
+                {
+                    IncludeItemTypes = new[]
+                    {
+                        SectionItemKind
+                    },
+                    DtoOptions = dtoOptions,
+                    IsPlayed = isPlayed,
+                    OrderBy = new [] { (ItemSortBy.DateCreated, SortOrder.Descending) },
+                    Limit = 16
+                });
+            }).DistinctBy(x => x.Id).OrderByDescending(x => GetSortDateForItem(x, user, dtoOptions)).Take(16);
+        }
+
         protected override DateTime GetSortDateForItem(BaseItem item, User? user, DtoOptions dtoOptions)
         {
             DateTime? dateCreated = null;
