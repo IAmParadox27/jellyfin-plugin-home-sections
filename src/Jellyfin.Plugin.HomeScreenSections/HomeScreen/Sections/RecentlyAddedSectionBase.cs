b4081a945a325267d408b78f0d9961ac871db165
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-10-29 23:07:07 +0000

    Another minor optimisation to recently added section

--- src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/RecentlyAddedSectionBase.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/RecentlyAddedSectionBase.cs
@@ -82,18 +82,7 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections
                 .Where(x => x.CollectionType == CollectionTypeOptions)
                 .ToArray();
 
-            IEnumerable<BaseItem> recentlyAddedItems = folders.SelectMany(x =>
-            {
-                return m_libraryManager.GetItemList(new InternalItemsQuery(user)
-                {
-                    IncludeItemTypes = new[]
-                    {
-                        SectionItemKind
-                    },
-                    DtoOptions = dtoOptions,
-                    IsPlayed = isPlayed
-                });
-            }).OrderByDescending(x => GetSortDateForItem(x, user, dtoOptions)).Take(16);
+            IEnumerable<BaseItem> recentlyAddedItems = GetItems(user, dtoOptions, folders, isPlayed);
             
             return new QueryResult<BaseItemDto>(Array.ConvertAll(recentlyAddedItems.ToArray(),
                 i => m_dtoService.GetBaseItemDto(i, dtoOptions, user)));
@@ -148,6 +137,26 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections
                 ViewMode = DefaultViewMode,
                 AllowHideWatched = true
             };
+        }
+
+        protected virtual IEnumerable<BaseItem> GetItems(User? user, DtoOptions dtoOptions, VirtualFolderInfo[] folders, bool? isPlayed)
+        {
+            // Default behaviour is to get the 16 most recently added items from each library that matches, then order that by date created and take 16.
+            // The reason we do this is to ensure that we always get 16 items, even if there is only 1 library that matches our type.
+            return folders.SelectMany(x => m_libraryManager.GetItemList(new InternalItemsQuery(user)
+            {
+                IncludeItemTypes = new[]
+                {
+                    SectionItemKind
+                },
+                DtoOptions = dtoOptions,
+                IsPlayed = isPlayed,
+                OrderBy = new [] { (ItemSortBy.DateCreated, SortOrder.Descending) },
+                Limit = 16,
+                IsMissing = false
+            })).DistinctBy(x => x.Id)
+            .OrderByDescending(x => GetSortDateForItem(x, user, dtoOptions))
+            .Take(16);
         }
         
         protected virtual DateTime GetSortDateForItem(BaseItem item, User? user, DtoOptions dtoOptions)
