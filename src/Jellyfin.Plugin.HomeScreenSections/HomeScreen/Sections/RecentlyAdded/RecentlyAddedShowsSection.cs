717d1299a0889937cf42a948a9202b1729370245
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-10-31 19:45:45 +0000

    Added some debug logs to help with #125

--- src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/RecentlyAdded/RecentlyAddedShowsSection.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/RecentlyAdded/RecentlyAddedShowsSection.cs
@@ -4,11 +4,14 @@ using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections.RecentlyAdded
 {
     public class RecentlyAddedShowsSection : RecentlyAddedSectionBase
     {
+        private readonly ILogger<RecentlyAddedShowsSection> m_logger;
+        
         public override string? Section => "RecentlyAddedShows";
 
         public override string? DisplayText { get; set; } = "Recently Added Shows";
@@ -30,8 +33,9 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections.RecentlyAdded
         public RecentlyAddedShowsSection(IUserViewManager userViewManager,
             IUserManager userManager,
             ILibraryManager libraryManager,
-            IDtoService dtoService) : base(userViewManager, userManager, libraryManager, dtoService)
+            IDtoService dtoService, ILogger<RecentlyAddedShowsSection> logger) : base(userViewManager, userManager, libraryManager, dtoService)
         {
+            m_logger = logger;
         }
 
         protected override IEnumerable<BaseItem> GetItems(User? user, DtoOptions dtoOptions, VirtualFolderInfo[] folders, bool? isPlayed)
@@ -69,14 +73,43 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections.RecentlyAdded
                     Limit = 1
                 };
 
-                dateCreated = m_libraryManager.GetItemList(query).FirstOrDefault()?.DateCreated;
+                BaseItem? latestItemAddedForShow = m_libraryManager.GetItemList(query).FirstOrDefault();
+
+                dateCreated = latestItemAddedForShow?.DateCreated;
+                
+                m_logger.LogInformation($"Show '{series.Name}' has been sorted based on episode '{latestItemAddedForShow?.Name}' which has date created of: {dateCreated}.");
+                
+                // This is debug code to help with testing the issue.
+                // It is slow, so it should only be used when debugging.
+                InternalItemsQuery debugQuery = new InternalItemsQuery(user)
+                {
+                    AncestorWithPresentationUniqueKey = null,
+                    SeriesPresentationUniqueKey = seriesKey,
+                    IncludeItemTypes = new[] { BaseItemKind.Episode },
+                    OrderBy = new[] { (ItemSortBy.DateCreated, SortOrder.Descending) },
+                    DtoOptions = dtoOptions,
+                    IsMissing = false
+                };
+                IReadOnlyList<BaseItem> allEpisodesInShow = m_libraryManager.GetItemList(debugQuery);
+
+                if (allEpisodesInShow.Max(x => x.DateCreated) != latestItemAddedForShow?.DateCreated)
+                {
+                    m_logger.LogWarning($"After getting all episodes for the show and getting the largest DateCreated value, the value is not the same as the one found using the quicker query. This is a bug.");
+                }
             }
             else if (item is Season season)
             {
-                dateCreated = season.GetEpisodes(user, dtoOptions, false).Max(x => x.DateCreated);
+                List<BaseItem>? seasonEpisodes = season.GetEpisodes(user, dtoOptions, false);
+                dateCreated = (seasonEpisodes?.Any() ?? false) ? seasonEpisodes.Max(x => x.DateCreated) : null;
+                
+                m_logger.LogInformation($"Season '{season.Name}' has been sorted based on an episode having a date created of: {dateCreated}.");
             }
 
-            dateCreated ??= base.GetSortDateForItem(item, user, dtoOptions);
+            if (dateCreated == null)
+            {
+                dateCreated = base.GetSortDateForItem(item, user, dtoOptions);
+                m_logger.LogInformation($"Item '{item.Name}' has been sorted based on the default behaviour with a value of: {dateCreated}.");
+            }
             
             return dateCreated.Value;
         }
