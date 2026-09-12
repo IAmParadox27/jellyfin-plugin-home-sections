2b146edb1c182feb2d808bb6d8d2f8c5b69fb06a
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2026-09-12 20:50:08 +0100

    Another approach to what pr #278 is suggesting, possibly with better results.

--- src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/Latest/LatestShowsSection.cs
+++ src/Jellyfin.Plugin.HomeScreenSections/HomeScreen/Sections/Latest/LatestShowsSection.cs
@@ -69,19 +69,24 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections.Latest
                 .Where(x => x.CollectionType == CollectionTypeOptions)
                 .FilterToUserPermitted(m_libraryManager, user);
 
-            List<(Series Series, DateTime? LatestPremiereDate)> selectedSeries = new List<(Series, DateTime?)>();
-            int dayIncrement = 30;
-            DateTime currentDate = DateTime.Now;
-            DateTime stopDate = DateTime.Parse("01/01/1925"); // The first show ever was 1925 so this should be safe, we never expect to get as far back as this but we need an escape.
+            Dictionary<Guid, Series> hydratedShowCache = new Dictionary<Guid, Series>();
+            
+            List<(Guid SeriesId, DateTime LatestPremiereDate, string UniqueKey)> selectedSeries = new List<(Guid, DateTime, string)>();
+            int episodeIncrement = 200;
+            int currentIndex = 0;
+            int maxResults = 16; // Adding this as a var here so we can make it configurable easier at a later date.
             bool continueSearching = true;
             
+            List<VirtualFolderInfo> foldersToQuery = folders.ToList();
             do
             {
                 // Single query: Get recent episodes, limited but enough to find 16 unique series
                 // Fetch more episodes to account for multiple episodes per series
-                var mainQuery = folders.Select(x =>
+                List<(string FolderId, IEnumerable<BaseItem> Items, int Count)> mainQuery = new List<(string, IEnumerable<BaseItem>, int)>();
+                List<VirtualFolderInfo> foldersToQueryCopy = foldersToQuery.ToList();
+                foreach (VirtualFolderInfo virtualFolder in foldersToQueryCopy)
                 {
-                    var item = m_libraryManager.GetParentItem(Guid.Parse(x.ItemId), user?.Id);
+                    var item = m_libraryManager.GetParentItem(Guid.Parse(virtualFolder.ItemId), user?.Id);
 
                     if (item is not Folder folder)
                     {
@@ -92,68 +97,131 @@ namespace Jellyfin.Plugin.HomeScreenSections.HomeScreen.Sections.Latest
                     {
                         IncludeItemTypes = new[] { SectionItemKind },
                         OrderBy = new[] { (ItemSortBy.PremiereDate, SortOrder.Descending) },
-                        Limit = 200, // Enough to find 16 unique series even with multi-episode releases
+                        StartIndex = currentIndex,
+                        Limit = episodeIncrement,
                         IsVirtualItem = false,
                         IsPlayed = isPlayed,
                         Recursive = true,
                         ParentId = folder.Id,
-                        MaxPremiereDate = currentDate,
-                        MinPremiereDate = currentDate.Subtract(TimeSpan.FromDays(dayIncrement)),
-                        EnableTotalRecordCount = true // This might have to go
+                        EnableTotalRecordCount = false
                         // DtoOptions = new DtoOptions { Fields = Array.Empty<ItemFields>(), EnableImages = false }
                     });
 
-                    return (Items: items.Items, items.Items.Count, items.TotalRecordCount);
-                }).ToArray();
+                    bool includeResults = true;
+                    if (selectedSeries.Select(x => x.UniqueKey).Distinct().Count() >= maxResults) // Intentional double buffer to allow for duplicate presentation unique keys to be filtered down to a count greater than maxResults
+                    {
+                        // When the query for this folder has started returning episodes older than the oldest we've got saved, we can stop searching it.
+                        DateTime latestPremiereInQuery = items.Items
+                            .OfType<Episode>()
+                            .Where(x => !x.IsUnaired)
+                            .Select(x => x.PremiereDate)
+                            .DefaultIfEmpty(DateTime.MinValue)
+                            .Max() ?? DateTime.MinValue;
+                        
+                        if (latestPremiereInQuery < selectedSeries.Min(x => x.LatestPremiereDate))
+                        {
+                            foldersToQuery.Remove(virtualFolder); // Avoid needing to query this folder again.
+                            continue;
+                        }
+                    }
 
-                var recentEpisodes = mainQuery.SelectMany(x => x.Items).OfType<Episode>()
-                .Where(x => !x.IsUnaired)
-                .ToList();
-                
-                // Group by series and get the one with the latest premiere date per series
-                var seriesWithLatestEpisode = recentEpisodes
-                    .Select(ep => (Episode: ep, Series: ep.Series))
-                    .Where(x => x.Series != null)
-                    .GroupBy(x => x.Series!.Id)
-                    .Select(g => (
-                        Series: g.First().Series!,
-                        LatestPremiereDate: g.Max(x => x.Episode.PremiereDate)
-                    ))
-                    .OrderByDescending(x => x.LatestPremiereDate)
-                    .Take(16)
-                    .ToList();
-                
-                var seriesToAdd = seriesWithLatestEpisode.Where(x => selectedSeries.All(y => y.Series.Id != x.Series.Id)).ToList();
-                
-                selectedSeries.AddRange(seriesToAdd);
+                    mainQuery.Add((FolderId: virtualFolder.ItemId, Items: items.Items, Count: items.Items.Count));
 
-                if (selectedSeries.Count >= 16)
-                {
-                    continueSearching = false;
+                    // We don't need to continue this folder as it's exhausted.
+                    if (items.Items.Count() < episodeIncrement)
+                    {
+                        foldersToQuery.Remove(virtualFolder); // Avoid needing to query this folder again.
+                    }
                 }
-                
-                currentDate = currentDate.Subtract(TimeSpan.FromDays(dayIncrement));
-                
-                if (currentDate < stopDate)
+
+                var rawEpisodeResults = mainQuery.SelectMany(x => x.Items).OfType<Episode>()
+                    .ToList();
+
+                if (rawEpisodeResults.Count == 0)
                 {
                     break;
                 }
+                
+                currentIndex += episodeIncrement;
+
+                var recentEpisodes = rawEpisodeResults
+                .Where(x => !x.IsUnaired)
+                .ToList();
+                
+                m_libraryManager.GetItemList(new InternalItemsQuery(user)
+                {
+                    ItemIds = recentEpisodes.Select(ep => ep.SeriesId == Guid.Empty
+                        ? ep.Series?.Id ?? Guid.Empty
+                        : ep.SeriesId).Distinct().Where(x => !hydratedShowCache.Keys.Contains(x)).ToArray(),
+                    GroupByPresentationUniqueKey = false,
+                    EnableTotalRecordCount = false
+                }).OfType<Series>().ToList().ForEach(x => hydratedShowCache[x.Id] = x);
+                
+                // Group by series and get the one with the latest premiere date per series
+                var seriesWithLatestEpisode = recentEpisodes
+                    .Select(ep => (Episode: ep, SeriesId: ep.SeriesId == Guid.Empty ? ep.Series?.Id ?? Guid.Empty : ep.SeriesId))
+                    .Where(x => x.SeriesId != Guid.Empty)
+                    .GroupBy(x => x.SeriesId)
+                    .Where(g => hydratedShowCache.TryGetValue(g.Key, out _))
+                    .Select(g =>
+                    {
+                        Series series = hydratedShowCache[g.Key];
+                        
+                        return (
+                            SeriesId: g.Key,
+                            LatestPremiereDate: g.Max(x => x.Episode.PremiereDate ?? DateTime.MinValue),
+                            UniqueKey: series.GetPresentationUniqueKey() ?? g.Key.ToString()
+                        );
+                    })
+                    .OrderByDescending(x => x.LatestPremiereDate)
+                    .ToList();
+                
+                var seriesToAdd = seriesWithLatestEpisode.Where(x => selectedSeries.All(y => y.SeriesId != x.SeriesId)).ToList();
+                
+                // We've decided that at least one of the shows we found fits in the list, so lets add them and trim down to the best 16
+                selectedSeries.AddRange(seriesToAdd);
+
+                selectedSeries = selectedSeries
+                    .GroupBy(x => x.UniqueKey)
+                    .Select(g => g
+                        .OrderByDescending(x => x.LatestPremiereDate)
+                        .First())
+                    .OrderByDescending(x => x.LatestPremiereDate)
+                    .Take(maxResults)
+                    .ToList();
+                
+                if (foldersToQuery.Count == 0)
+                {
+                    continueSearching = false;
+                }
             } while (continueSearching);
             
-            // Fetch the full series objects with proper DtoOptions for images
-            var seriesIds = selectedSeries.OrderByDescending(x => x.LatestPremiereDate).Select(x => x.Series.Id);
-            var seriesIdArray = seriesIds.ToArray();
-            var seriesItems = m_libraryManager.GetItemList(new InternalItemsQuery(user)
-            {
-                ItemIds = seriesIdArray,
-                DtoOptions = dtoOptions
-            });
-            
             // Maintain the order from our sorted list
-            var orderedSeries = seriesIdArray
-                .Select(id => seriesItems.FirstOrDefault(s => s.Id == id))
-                .Where(s => s != null)
+            Dictionary<Guid, DateTime> latestDates = selectedSeries
+                .ToDictionary(x => x.SeriesId, x => x.LatestPremiereDate);
+
+            // Fetch the full series objects with proper DtoOptions for images
+            List<Series> hydratedSeries = m_libraryManager
+                .GetItemList(new InternalItemsQuery(user)
+                {
+                    ItemIds = latestDates.Keys.ToArray(),
+                    DtoOptions = dtoOptions,
+                    GroupByPresentationUniqueKey = false,
+                    EnableTotalRecordCount = false
+                })
+                .OfType<Series>()
                 .ToList();
+
+            Series[] orderedSeries = hydratedSeries
+                .GroupBy(x => string.IsNullOrEmpty(x.PresentationUniqueKey)
+                    ? x.Id.ToString()
+                    : x.PresentationUniqueKey)
+                .OrderByDescending(g => g.Max(x => latestDates[x.Id]))
+                .Select(g => g
+                    .OrderByDescending(x => latestDates[x.Id])
+                    .First())
+                .Take(maxResults)
+                .ToArray();
             
             return new QueryResult<BaseItemDto>(Array.ConvertAll(orderedSeries.ToArray(),
                 i => m_dtoService.GetBaseItemDto(i!, dtoOptions, user)));
